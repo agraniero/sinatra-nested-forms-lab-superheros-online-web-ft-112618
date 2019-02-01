11b8678b43735563b965979040b2e18ea7fda2d7
Anthony Graniero <ananfro@gmail.com>
2019-02-01 12:55:17 +0000

    Automatically backed up by Learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,14 @@
 class Team
   attr_accessor :name, :motto
   
+  @@all = []
+  
   def initialize(args)
     @name = args[:name]
     @motto = motto[:motto]
+  end
+  
+  def save
+    @@all << self
   end
 end
