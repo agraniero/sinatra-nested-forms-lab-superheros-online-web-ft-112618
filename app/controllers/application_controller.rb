db08fb457dee0c451b3eafaac5dcc71d065885ea
Anthony Graniero <ananfro@gmail.com>
2019-02-01 13:09:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,16 @@ class App < Sinatra::Base
   end
 
   post '/teams' do
-    @teams = 
+    @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+    members = params[:team][:members]
+    @super_heros = members.collect do |member_params|
+      SuperHero.new({
+        name: member_params[:name] 
+        power: member_params[:power]
+        bio: member_params[:bio]
+      })
+    end
+    erb :team
 end
+
+
