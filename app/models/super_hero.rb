ce4464d140e995ed2166a3f4a0bfaa309ed9b92e
Anthony Graniero <ananfro@gmail.com>
2019-02-01 12:50:24 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,3 +1,19 @@
 class SuperHero
-  attr_accessor :name, :power, :biography
+  attr_accessor :name, :power, :bio
+    
+    @@all = []
+    
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @bio = args[:bio]
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.clear
+    @@all.clear
+  end
 end
